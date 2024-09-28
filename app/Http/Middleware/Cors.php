62aaa5f9bd8167b4bea8630f5f69b8682ce6fecc
hayatosuzuki88 <ha.ya.to.n.su.zu.ki8810@gmail.com>
2024-09-29 01:36:33 +0900

    version2.7

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Cors
+{
+    public function handle($request, Closure $next)
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST')
+            ->header('Access-Control-Allow-Headers', 'Accept, X-Requested-With, Origin, Content-Type');
+    }
+}
